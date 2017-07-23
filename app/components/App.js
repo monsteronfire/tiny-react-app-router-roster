c8d2f5e03891d063dadb61b2bdfb0f4bcaacff1e
Shaii Ong <monsteronfire@gmail.com>
2017-07-24 00:43:00 +0800

    Add BrowserRouter

--- app/components/App.js
+++ app/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactRouter from 'react-router-dom';
 import Nav from './Nav';
+import Roster from './Roster';
+import Schedule from './Schedule';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='main-container'>
-        <BrowserRouter>
+      <BrowserRouter>
+        <div className='main-container'>
           <Nav/>
-        </BrowserRouter>
-      </div>
+          <Route path='/roster' component={Roster} />
+          <Route path='/schedule' component={Schedule} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
---
+++ app/components/Roster.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Roster extends React.Component {
+   render() {
+     return (
+       <div className='roster-wrapper'>
+         Roster
+       </div>
+     )
+   }
+}
+
+export default Roster;
