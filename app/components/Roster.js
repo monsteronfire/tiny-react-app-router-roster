6cb67466276a6b4692ac2e17e022288e88c256ce
Shaii Ong <monsteronfire@gmail.com>
2017-07-25 01:09:10 +0800

    Add PlayerAPI

--- app/components/Roster.js
+++ app/components/Roster.js
@@ -3,6 +3,21 @@ import { Switch, Route }  from 'react-router-dom';
 import FullRoster from './FullRoster';
 import Player from './Player';
 
+const PlayerAPI = {
+  players: [
+    { number: 1, name: 'Tidus', position: 'G' },
+    { number: 2, name: 'Kimari', position: 'H' },
+    { number: 3, name: 'Wakka', position: 'I' },
+  ],
+  all: function () {
+    return this.players;
+  },
+  get: function(id) {
+    const isPlayer = p => p.number === id;
+    return this.players.find(isPlayer);
+  }
+};
+
 class Roster extends React.Component {
    render() {
      return (
